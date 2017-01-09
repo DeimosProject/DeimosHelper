a87ce61a76823e20be4fbf806d6a787dd7519e4d
sttv <sttv-pc@mail.ru>
2017-01-09 13:15:11 +0300

    Money tests #1

--- src/Helper/Helpers/Money.php
+++ src/Helper/Helpers/Money.php
@@ -11,52 +11,33 @@ class Money extends AbstractHelper
      * Format amount of money based on locale
      *
      * @param  string $amount
-     * @param  string $currency
      * @param  string $locale [ de tr pt it nl fr ru en ]
      *
      * @return string
      */
-    public function format($amount, $currency = '€', $locale = 'en')
+    public function format($amount, $locale = 'en')
     {
-        $symbols = array('€', '$', '£', '¥', '₤', 'kr', '₺');
         switch ($locale)
         {
             case 'de':
             case 'tr':
             case 'pt':
-                $amount = number_format($amount, 2, ',', '.');
-                $space  = ' ';
-                $format = is_string($currency) ? $amount . $space . $currency : $amount;
-                break;
-
             case 'it':
             case 'nl':
                 $amount = number_format($amount, 2, ',', '.');
-                $space  = ' ';
-                $format = is_string($currency) ? $currency . $space . $amount : $amount;
                 break;
 
             case 'fr':
-                $amount = number_format($amount, 2, ',', ' ');
-                $space  = ' ';
-                $format = is_string($currency) ? $amount . $space . $currency : $amount;
-                break;
-
             case 'ru':
                 $amount = number_format($amount, 2, ',', ' ');
-                $space  = in_array($currency, $symbols) ? '' : ' ';
-                $format = is_string($currency) ? $amount . $space . $currency : $amount;
                 break;
 
-            default:
             case 'en':
                 $amount = number_format($amount, 2, '.', ',');
-                $space  = in_array($currency, $symbols) ? '' : ' ';
-                $format = is_string($currency) ? $currency . $space . $amount : $amount;
                 break;
         }
 
-        return $format;
+        return $amount;
     }
 
 }
---
+++ tests/Tests/ModeyTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+class MoneyTest extends \DeimosTest\TestsSetUp
+{
+
+    protected $moneyValue = 1234.15;
+
+    public function testFormatEn()
+    {
+        $this->assertEquals(
+            $this->helper->money()->format($this->moneyValue),
+            number_format($this->moneyValue, 2, '.', ',')
+        );
+    }
+    public function testFormatDe()
+    {
+        $this->assertEquals(
+            $this->helper->money()->format($this->moneyValue, 'de'),
+            number_format($this->moneyValue, 2, ',', '.')
+        );
+    }
+    public function testFormatRu()
+    {
+        $this->assertEquals(
+            $this->helper->money()->format($this->moneyValue, 'ru'),
+            number_format($this->moneyValue, 2, ',', ' ')
+        );
+    }
+
+}
