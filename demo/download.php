533fdd2a8f40efee7ff77a22c0198d68f9ef26fe
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-30 08:44:10 +0300

    patch download.php

--- demo/download.php
+++ demo/download.php
@@ -5,7 +5,11 @@ include_once __DIR__ . '/../vendor/autoload.php';
 $builder = new \Deimos\Builder\Builder();
 $helper  = new \Deimos\Helper\Helper($builder);
 
+$name = $helper->str()->translit('MVC Example Администрирование локальных сетей.zip');
+
 var_dump($helper->stream()->download(
     'https://fktpm.ru/get_file/0My03',
-    $helper->str()->translit('MVC Example Администрирование локальных сетей.zip')
+    $name
 ));
+
+register_shutdown_function('unlink', $name);
